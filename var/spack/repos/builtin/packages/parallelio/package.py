85e7bbd3b93a8abc3c45986a850d97687386e594
Edward Hartnett <38856240+edwardhartnett@users.noreply.github.com>
2021-09-15 07:50:45 -0600

    trying to fix python warning by removing whitespace

--- var/spack/repos/builtin/packages/parallelio/package.py
+++ var/spack/repos/builtin/packages/parallelio/package.py
@@ -20,7 +20,7 @@ class Parallelio(CMakePackage):
     version('2_5_3', sha256='63ae300d7b2e8790a272d442cb2856ff053810a05a9c37d28228fc4ebde1f5f5')
     version('2_5_2', sha256='378e6d01dbfb9e99a913be814d3a4f04f93a3bb9f860468ccaf199ed3687acac')
     version('2_5_0', sha256='685cfe16d5d308b65e86242bcb8acafdfabd1bf4bff964789745c79caea9dca1')
-    
+
     variant('pnetcdf', default=False, description='enable pnetcdf')
 
     depends_on('mpi')
