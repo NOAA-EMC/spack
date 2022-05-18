3afcf566129b661cb27cc96c39083b253de9c8a1
kgerheiser <kgerheiser@icloud.com>
2022-05-18 10:58:36 -0400

    Cleanup and fix for packages key in container

--- lib/jcsda-emc/spack-stack/stack/cmd/stack_cmds/create.py
+++ lib/jcsda-emc/spack-stack/stack/cmd/stack_cmds/create.py
@@ -3,18 +3,14 @@ import spack.cmd.modules
 import os
 import logging
 from spack.extensions.stack.stack_env import StackEnv, stack_path, app_path
+from spack.extensions.stack.container_env import StackContainer
 import shutil
 import llnl.util.tty as tty
 import spack.util.spack_yaml as syaml
 
-description = "Create spack-stack environment"
+description = "Create spack-stack environment (env or container)"
 section = "spack-stack-env"
 level = "long"
-
-subcommands = [
-    'create',
-]
-
 
 default_env_name = 'default'
 default_env_path = os.path.join(stack_path(), 'envs')
@@ -142,67 +138,6 @@ def setup_create_parser(subparser):
     setup_container_parser(container_parser)
 
 
-container_path = os.path.join(stack_path(), 'configs', 'containers')
-app_path
-
-
-class StackContainer():
-    """Represents an abstract container. It takes in a
-    conatiner template (spack.yaml), the specs from an app, and
-    its packages.yaml versions.
-    """
-
-    def __init__(self, container, app, name, dir, base_packages) -> None:
-        if os.path.isabs(container):
-            self.container_path = container
-        elif os.path.exists(os.path.join(container_path, container)):
-            self.container_path = os.path.join(container_path, container)
-        else:
-            raise Exception("Invalid container")
-
-        if os.path.isabs(app):
-            self.app_path = app
-        elif os.path.exists(os.path.join(app_path, app)):
-            self.app_path = os.path.join(app_path, app)
-        else:
-            raise Exception("Invalid app")
-
-        basename = os.path.basename(self.container_path)
-        self.name = name if name else '{}.{}'.format(basename, app)
-
-        self.dir = dir
-        self.env_dir = os.path.join(self.dir, self.name)
-        self.base_packages = base_packages
-
-    def write(self):
-        """Merge base packages and app's spack.yaml into
-        output container file
-        """
-        app_env = os.path.join(self.app_path, 'spack.yaml')
-        sections = ['packages', 'specs']
-        with open(app_env, 'r') as f:
-            app_yaml = syaml.load_config(f)
-
-        with open(self.container_path, 'r') as f:
-            container_yaml = syaml.load_config(f)
-
-        with open(self.base_packages, 'r') as f:
-            packages_yaml = syaml.load_config(f)
-
-        if 'packages' not in container_yaml['spack']:
-            container_yaml['spack']['packages'] = {}
-
-        container_yaml['spack']['packages'] = spack.config.merge_yaml(
-                container_yaml['spack']['packages'], packages_yaml)
-
-        container_yaml = spack.config.merge_yaml(container_yaml, app_yaml)
-
-        os.makedirs(self.env_dir, exist_ok=True)
-
-        with open(os.path.join(self.env_dir, 'spack.yaml'), 'w') as f:
-            syaml.dump_config(container_yaml, stream=f)
-
-
 def container_create(args):
     """Create pre-configured container"""
 
@@ -218,6 +153,7 @@ def container_create(args):
             raise Exception('Env: {} already exists'.format(env_dir))
 
     container.write()
+    tty.msg('Created container {}'.format(env_dir))
 
 
 def dict_from_args(args):
