8abbd6153f091659e76e953aa7bd66d13c5d12d9
kgerheiser <kgerheiser@icloud.com>
2022-05-18 09:44:24 -0400

    Add container subcommand for stack create.
    Container takes a container template, and an app. The specs and packages are extracted from the app, along with a base packages.yaml which defaults to common/packages.yaml.

--- lib/jcsda-emc/spack-stack/stack/cmd/stack.py
+++ lib/jcsda-emc/spack-stack/stack/cmd/stack.py
@@ -5,16 +5,16 @@ import logging
 import shutil
 import llnl.util.tty as tty
 import spack.util.spack_yaml as syaml
-from spack.extensions.stack.env import StackEnv, stack_path
-
-logging.basicConfig(level=logging.INFO)
+from spack.extensions.stack.stack_env import StackEnv, stack_path
+from spack.extensions.stack.cmd.stack_cmds.create import setup_create_parser, stack_create
 
 description = "Create spack-stack environment"
 section = "spack-stack-env"
 level = "long"
 
 subcommands = [
-    'create',
+    'env',
+    'container'
 ]
 
 
@@ -22,155 +22,18 @@ default_env_name = 'default'
 default_env_path = os.path.join(stack_path(), 'envs')
 default_packages = os.path.join(stack_path(),
                                 'configs', 'common', 'packages.yaml')
-
-
-def stack_create_setup_parser(subparser):
-    subparser.add_argument(
-        '--name', type=str, required=False, default=default_env_name,
-        help='Environment name, defaults to {}.'.format(default_env_name) +
-        ' Environment will be in <prefix>/<name>'
-    )
-
-    subparser.add_argument(
-        '--dir', type=str, required=False, default=default_env_path,
-        help='Environment will be placed in <dir>/<name>.'
-        ' Default is {}.'.format(default_env_path)
-    )
-
-    subparser.add_argument(
-        '--app', type=str, required=False, dest='app', default=None,
-        help='Either a named app in (configs/apps) or path to spack.yaml'
-        ' to be used as the base for further customization.'
-    )
-
-    subparser.add_argument(
-        '--site', type=str, required=False, default='default',
-        help='Pre-configured platform to build for (e.g. hera, jet, orion)'
-        ' otherwise no machine-specific config files are included'
-    )
-
-    subparser.add_argument(
-        '--envs-file', type=str, required=False, default=None,
-        help='Create environments from envs.yaml file.'
-        ' Other command-line options will be ignored'
-    )
-
-    subparser.add_argument(
-        '--overwrite', action='store_true', required=False, default=False,
-        help='Overwrite existing environment, if it exists.'
-        ' Warning this is dangerous.'
-    )
-
-    subparser.add_argument(
-        '--base-packages', type=str, required=False, default=default_packages,
-        help='Base packages.yaml.'
-        ' Defaults to {}'.format(default_packages)
-    )
-
-    subparser.add_argument(
-        '--prefix', type=str, required=False, default=None,
-        help='Install prefix.'
-    )
-
-    subparser.add_argument(
-        '--module-prefix', type=str, required=False, default='modulefiles',
-        help='Module install prefix. Modules will be placed relative to'
-             'install prefix or absolute path if given.'
-    )
-
-    subparser.add_argument(
-        '--no-common', action='store_true',
-        help='Do not use common config files.'
-    )
-
-    subparser.add_argument(
-        '--no-includes', action='store_true', required=False, default=None,
-        help='Copy base-packges directly into spack.yaml (no includes)'
-        ' Useful in containers that cannot have includes.'
-        ' Warning this will break with packages that have "::" because'
-        ' of a bug with Spack the replaces it with a quote when used in'
-        ' a spack.yaml.'
-    )
-
-
-def stack_create(args):
-    """Create pre-configured Spack environment.
-
-    Args: args
-
-    Returns:
-
-    """
-    site = args.site
-    app = args.app
-    name = args.name
-    dir = args.dir
-    envs_file = args.envs_file
-    overwrite = args.overwrite
-    install_prefix = args.prefix
-    module_prefix = args.module_prefix
-    no_common = args.no_common
-
-    base_packages = args.base_packages
-    no_includes = args.no_includes
-
-    env_dir = os.path.join(dir, name)
-    if os.path.exists(env_dir):
-        if overwrite:
-            tty.msg('Env {} exists. Overwriting...'.format(env_dir))
-            shutil.rmtree(env_dir)
-
-    if envs_file:
-        logging.debug('Creating envs from envs_file')
-        with open(envs_file, 'r') as f:
-            site_envs = syaml.load_config(stream=f)
-
-        envs = site_envs['envs']
-        for env in envs:
-            stack_env = StackEnv(**env['env'])
-            stack_env.write()
-    else:
-        logging.debug('Creating envs from command-line args')
-        stack_env = StackEnv(name=name, dir=dir, site=site, app=app,
-                             base_packages=base_packages,
-                             no_includes=no_includes,
-                             install_prefix=install_prefix,
-                             module_prefix=module_prefix,
-                             no_common=no_common
-                             )
-        stack_env.write()
-
-
-#: Dictionary mapping subcommand names and aliases to functions
-subcommand_functions = {}
+container_path = os.path.join(stack_path(), 'configs' 'containers')
 
 
 def setup_parser(subparser):
     sp = subparser.add_subparsers(metavar='SUBCOMMAND', dest='stack_command')
-
-    for name in subcommands:
-        if isinstance(name, (list, tuple)):
-            name, aliases = name[0], name[1:]
-        else:
-            aliases = []
-
-        # add commands to subcommands dict
-        function_name = 'stack_%s' % name
-        function = globals()[function_name]
-        for alias in [name] + aliases:
-            subcommand_functions[alias] = function
-
-        # make a subparser and run the command's setup function on it
-        setup_parser_cmd_name = 'stack_%s_setup_parser' % name
-        setup_parser_cmd = globals()[setup_parser_cmd_name]
-
-        subsubparser = sp.add_parser(
-            name, aliases=aliases, help=setup_parser_cmd.__doc__)
-        setup_parser_cmd(subsubparser)
+    create_parser = sp.add_parser('create',
+                                  help='Create spack-stack env or container.')
+    setup_create_parser(create_parser)
 
 
 # Main command that calls subcommands
 def stack(parser, args):
     """Look for a function called environment_<name> and call it."""
-    action = subcommand_functions[args.stack_command]
-    action(args)
+    if args.stack_command == 'create':
+        stack_create(parser, args)
---
+++ lib/jcsda-emc/spack-stack/stack/cmd/stack_cmds/create.py
@@ -0,0 +1,278 @@
+import spack.cmd.common.arguments
+import spack.cmd.modules
+import os
+import logging
+from spack.extensions.stack.stack_env import StackEnv, stack_path, app_path
+import shutil
+import llnl.util.tty as tty
+import spack.util.spack_yaml as syaml
+
+description = "Create spack-stack environment"
+section = "spack-stack-env"
+level = "long"
+
+subcommands = [
+    'create',
+]
+
+
+default_env_name = 'default'
+default_env_path = os.path.join(stack_path(), 'envs')
+default_packages = os.path.join(stack_path(),
+                                'configs', 'common', 'packages.yaml')
+
+
+def setup_container_parser(subparser):
+    subparser.add_argument(
+        'container', help='Container template to use in configs/containers')
+
+    subparser.add_argument(
+        '--app', type=str, required=True, dest='app', default=None,
+        help='Either a named app in (configs/apps) or path to spack.yaml'
+        ' to be used as the base for further customization.'
+    )
+
+    subparser.add_argument(
+        '--name', type=str, required=False, default=None,
+        help='Environment name, defaults to {}.'.format(default_env_name) +
+        ' Environment will be in <prefix>/<name>'
+    )
+
+    subparser.add_argument(
+        '--dir', type=str, required=False, default=default_env_path,
+        help='Environment will be placed in <dir>/<name>/contents.'
+        ' Default is {}.'.format(default_env_path)
+    )
+
+    subparser.add_argument(
+        '--overwrite', action='store_true', required=False, default=False,
+        help='Overwrite existing environment, if it exists.'
+        ' Warning this is dangerous.'
+    )
+
+    subparser.add_argument(
+        '--base-packages', type=str, required=False, default=default_packages,
+        help='Base packages.yaml.'
+        ' Defaults to {}'.format(default_packages)
+    )
+
+    subparser.add_argument(
+        '--specs', type=str, required=False, default=default_packages,
+        help='A specs.yaml or individual spec'
+    )
+
+
+def setup_env_parser(subparser):
+    subparser.add_argument(
+        '--name', type=str, required=False, default=default_env_name,
+        help='Environment name, defaults to {}.'.format(default_env_name) +
+        ' Environment will be in <prefix>/<name>'
+    )
+
+    subparser.add_argument(
+        '--dir', type=str, required=False, default=default_env_path,
+        help='Environment will be placed in <dir>/<name>/contents.'
+        ' Default is {}.'.format(default_env_path)
+    )
+
+    subparser.add_argument(
+        '--app', type=str, required=False, dest='app', default=None,
+        help='Either a named app in (configs/apps) or path to spack.yaml'
+        ' to be used as the base for further customization.'
+    )
+
+    subparser.add_argument(
+        '--site', type=str, required=False, default=None,
+        help='Pre-configured platform to build for (e.g. hera, jet, orion)'
+        ' otherwise no machine-specific config files are included'
+    )
+
+    subparser.add_argument(
+        '--envs-file', type=str, required=False, default=None,
+        help='Create environments from envs.yaml file.'
+        ' Other command-line options will be ignored'
+    )
+
+    subparser.add_argument(
+        '--overwrite', action='store_true', required=False, default=False,
+        help='Overwrite existing environment, if it exists.'
+        ' Warning this is dangerous.'
+    )
+
+    subparser.add_argument(
+        '--base-packages', type=str, required=False, default=default_packages,
+        help='Base packages.yaml.'
+        ' Defaults to {}'.format(default_packages)
+    )
+
+    subparser.add_argument(
+        '--prefix', type=str, required=False, default=None,
+        help='Install prefix.'
+    )
+
+    subparser.add_argument(
+        '--module-prefix', type=str, required=False, default='modulefiles',
+        help='Module install prefix. Modules will be placed relative to'
+             'install prefix or absolute path if given.'
+    )
+
+    subparser.add_argument(
+        '--no-common', action='store_true',
+        help='Do not use common config files.'
+    )
+
+    subparser.add_argument(
+        '--no-includes', action='store_true', required=False, default=None,
+        help='Copy base-packges directly into spack.yaml (no includes)'
+        ' Useful in containers that cannot have includes.'
+        ' Warning this will break with packages that have "::" because'
+        ' of a bug with Spack the replaces it with a quote when used in'
+        ' a spack.yaml.'
+    )
+
+
+def setup_create_parser(subparser):
+    sp = subparser.add_subparsers(metavar='SUBCOMMAND', dest='env_type')
+
+    env_parser = sp.add_parser('env', help='Create local Spack environment')
+    container_parser = sp.add_parser('container', help='Create container.')
+
+    setup_env_parser(env_parser)
+
+    setup_container_parser(container_parser)
+
+
+container_path = os.path.join(stack_path(), 'configs', 'containers')
+app_path
+
+
+class StackContainer():
+    """Represents an abstract container. It takes in a
+    conatiner template (spack.yaml), the specs from an app, and
+    its packages.yaml versions.
+    """
+
+    def __init__(self, container, app, name, dir, base_packages) -> None:
+        if os.path.isabs(container):
+            self.container_path = container
+        elif os.path.exists(os.path.join(container_path, container)):
+            self.container_path = os.path.join(container_path, container)
+        else:
+            raise Exception("Invalid container")
+
+        if os.path.isabs(app):
+            self.app_path = app
+        elif os.path.exists(os.path.join(app_path, app)):
+            self.app_path = os.path.join(app_path, app)
+        else:
+            raise Exception("Invalid app")
+
+        basename = os.path.basename(self.container_path)
+        self.name = name if name else '{}.{}'.format(basename, app)
+
+        self.dir = dir
+        self.env_dir = os.path.join(self.dir, self.name)
+        self.base_packages = base_packages
+
+    def write(self):
+        """Merge base packages and app's spack.yaml into
+        output container file
+        """
+        app_env = os.path.join(self.app_path, 'spack.yaml')
+        sections = ['packages', 'specs']
+        with open(app_env, 'r') as f:
+            app_yaml = syaml.load_config(f)
+
+        with open(self.container_path, 'r') as f:
+            container_yaml = syaml.load_config(f)
+
+        with open(self.base_packages, 'r') as f:
+            packages_yaml = syaml.load_config(f)
+
+        if 'packages' not in container_yaml['spack']:
+            container_yaml['spack']['packages'] = {}
+
+        container_yaml['spack']['packages'] = spack.config.merge_yaml(
+                container_yaml['spack']['packages'], packages_yaml)
+
+        container_yaml = spack.config.merge_yaml(container_yaml, app_yaml)
+
+        os.makedirs(self.env_dir, exist_ok=True)
+
+        with open(os.path.join(self.env_dir, 'spack.yaml'), 'w') as f:
+            syaml.dump_config(container_yaml, stream=f)
+
+
+def container_create(args):
+    """Create pre-configured container"""
+
+    container = StackContainer(args.container, args.app, args.name,
+                               args.dir, args.base_packages)
+
+    env_dir = container.env_dir
+    if os.path.exists(env_dir):
+        if args.overwrite:
+            tty.msg('Env {} exists. Overwriting...'.format(env_dir))
+            shutil.rmtree(env_dir)
+        else:
+            raise Exception('Env: {} already exists'.format(env_dir))
+
+    container.write()
+
+
+def dict_from_args(args):
+    dict = {}
+    dict['site'] = args.site
+    dict['app'] = args.app
+    dict['name'] = args.name
+    dict['envs_file'] = args.envs_file
+    dict['install_prefix'] = args.prefix
+    dict['module_prefix'] = args.module_prefix
+    dict['no_common'] = args.no_common
+    dict['base_packages'] = args.base_packages
+    dict['no_incldues'] = args.no_includes
+    dict['dir'] = args.dir
+
+    return dict
+
+
+def env_create(args):
+    """Create pre-configured Spack environment.
+
+    Args: args
+
+    Returns:
+
+    """
+
+    stack_settings = dict_from_args(args)
+    stack_env = StackEnv(**stack_settings)
+
+    env_dir = stack_env.env_dir()
+    if os.path.exists(env_dir):
+        if args.overwrite:
+            tty.msg('Env {} exists. Overwriting...'.format(env_dir))
+            shutil.rmtree(env_dir)
+        else:
+            raise Exception('Env: {} already exists'.format(env_dir))
+
+    if args.envs_file:
+        logging.debug('Creating envs from envs_file')
+        with open(args.envs_file, 'r') as f:
+            site_envs = syaml.load_config(stream=f)
+
+        envs = site_envs['envs']
+        for env in envs:
+            stack_env = StackEnv(**env['env'])
+            stack_env.write()
+    else:
+        logging.debug('Creating envs from command-line args')
+        stack_env = StackEnv(**stack_settings)
+        stack_env.write()
+
+
+def stack_create(parser, args):
+    if args.env_type == 'env':
+        env_create(args)
+    elif args.env_type == 'container':
+        container_create(args)
