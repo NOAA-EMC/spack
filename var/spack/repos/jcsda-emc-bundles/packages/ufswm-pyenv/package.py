f9641e0977dd13b3ff3816846e61e76aeba04b0f
kgerheiser <kgerheiser@icloud.com>
2022-05-24 12:11:26 -0400

    Add ufswm-pyenv and gw-pyenv

---
+++ var/spack/repos/jcsda-emc-bundles/packages/ufswm-pyenv/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class GwPyenv(BundlePackage):
+    """
+    Python development environment for the UFS weather model.
+    """
+
+    homepage = "https://github.com/NOAA-EMC/global-workflow"
+    git = "https://github.com/NOAA-EMC/global-workflow.git"
+    # There is no URL since there is no code to download.
+
+    maintainers = ['kgerheiser', 'Hang-Lei-NOAA']
+
+    depends_on('py-cython')
+    depends_on('py-cftime')
+    depends_on('py-h5py')
+    depends_on('py-numpy')
+    depends_on('py-pandas')
+    depends_on('py-python-dateutil')
+    depends_on('py-netcdf4')
