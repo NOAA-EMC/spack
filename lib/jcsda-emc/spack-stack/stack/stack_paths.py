40c5dc9d72b2ece49882651d5ce3b2df7547e3fc
kgerheiser <kgerheiser@icloud.com>
2022-07-15 11:31:59 -0400

    Test

--- lib/jcsda-emc/spack-stack/stack/stack_paths.py
+++ lib/jcsda-emc/spack-stack/stack/stack_paths.py
@@ -6,12 +6,20 @@ import spack
 # find relative config files. Assuming Spack is a submodule of
 # spack-stack.
 check_file = '.spackstack'
+stack_dir = os.path.dirname(spack.paths.spack_root)
 
 
-# Find spack-stack directory assuming this Spack instance
-# is a submodule of spack-stack.
+def is_stack_submodule():
+    return os.path.exists(os.path.join(stack_dir, check_file))
+
+
 def stack_path(*paths):
     stack_dir = os.path.dirname(spack.paths.spack_root)
+
+    if not is_stack_submodule():
+        print("Not a submodule of spack-stack, exiting")
+        exit(0)
+
     return os.path.join(stack_dir, *paths)
 
 
@@ -19,3 +27,4 @@ common_path = stack_path('configs', 'common')
 site_path = stack_path('configs', 'sites')
 container_path = stack_path('configs', 'containers')
 template_path = stack_path('configs', 'templates')
+default_env_path = stack_path('envs')
