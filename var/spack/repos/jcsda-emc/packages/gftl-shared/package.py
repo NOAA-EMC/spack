1434223fc402b45c80e0d92898c4c46f0e31c2ed
Matthew Thompson <matthew.thompson@nasa.gov>
2022-09-19 08:44:10 -0400

    Update GFE and MAPL packages

--- var/spack/repos/jcsda-emc/packages/gftl-shared/package.py
+++ var/spack/repos/jcsda-emc/packages/gftl-shared/package.py
@@ -13,16 +13,28 @@ class GftlShared(CMakePackage):
     """
 
     homepage = "https://github.com/Goddard-Fortran-Ecosystem/gFTL-shared"
-    git = "https://github.com/Goddard-Fortran-Ecosystem/gFTL-shared.git"
+    url      = "https://github.com/Goddard-Fortran-Ecosystem/gFTL-shared/archive/refs/tags/v1.5.0.tar.gz"
+    git      = "https://github.com/Goddard-Fortran-Ecosystem/gFTL-shared.git"
 
-    maintainers = ['kgerheiser', 'edwardhartnett', 'Hang-Lei-NOAA']
+    maintainers = ['mathomp4', 'tclune']
 
-    version('1.5.0', commit='6b60a685e59dc30d3a25ad0b5a1d0de9dc610b76', submodules=True)
-    version('1.4.1', commit='1c85cb233d195f3fb5eb51ceb362c3bf09f2d7c4', submodules=True)
-    version('1.3.6', commit='b4a11c3ae3f0a5d43f25c83ae9b508975fc48e6f', submodules=True)
-    version('1.3.5', commit='221940d8852e0f2b6366fa6bfc1f45ae1e9751f1', submodules=True)
-    version('1.3.3', commit='0351c67ab6f37c4815f0829aa1ca826c3d78e769', submodules=True)
-    version('1.3.0', commit='8f770ee380a99c72dbc525cff0699924be052848', submodules=True)
+    version('main', branch='main')
+
+    version('1.5.0', sha256='c19b8197cc6956d4a51a16f98b38b63c7bc9f784f1fd38f8e3949be3ea792356')
+    version('1.4.1', sha256='bb403f72e80aaac49ed5107f7c755ce5273c2e650bd5438a746228798eeced6c')
+    version('1.4.0', sha256='83a2474ae943d81d797460b18106874de14c39093efd4e35abb3f1b6ec835171')
+    version('1.3.6', sha256='d8cd1fc7b8c9a42fc44c8986f6b89e06589bef9b6718699e564dd506e101cf1f')
+    version('1.3.5', sha256='5cb421cf79a0505d21da6c25961dc7f9f108a4ff68a2ee8b5db39b2926a1133f')
+    version('1.3.4', sha256='02570edb08af379aa59d3a15296c0231701e114de273ce08804c718681555854')
+    version('1.3.3', sha256='40822130fc4eec9d34ba71cc0ee0a00fb7410e5ce4d2841cb405f192fb12ab3b')
+    version('1.3.2', sha256='142e94420986fa1bb3797bd4a0e61ca07cdd4d379465734bd25ec92032d769f0')
+    version('1.3.1', sha256='a71e164108847f32f37da505f604fc2a50f392a4fcdf9a7cfe8eaf775bed64d4')
+    version('1.3.0', sha256='979b00c4d531e701bf4346f662e3e4cc865124a97ca958637a53201d66d4ee43')
 
     depends_on('m4', type=('build', 'run'))
-    depends_on('git', type=('build'))
+    depends_on('cmake', type='build')
+    depends_on('gftl', type='build')
+
+    variant('build_type', default='Release',
+        description='The build type to build',
+        values=('Debug', 'Release'))
--- var/spack/repos/jcsda-emc/packages/mapl/package.py
+++ var/spack/repos/jcsda-emc/packages/mapl/package.py
@@ -1,9 +1,9 @@
-# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack.package import *
+from spack import *
 
 
 class Mapl(CMakePackage):
@@ -18,11 +18,20 @@ class Mapl(CMakePackage):
     """
 
     homepage = "https://github.com/GEOS-ESM/MAPL"
-    url = "https://github.com/GEOS-ESM/MAPL/archive/refs/tags/v2.8.1.tar.gz"
+    url      = "https://github.com/GEOS-ESM/MAPL/archive/refs/tags/v2.17.1.tar.gz"
+    git      = "https://github.com/GEOS-ESM/MAPL.git"
 
     maintainers = ["mathomp4", "kgerheiser", "climbfuji", "edwardhartnett", "Hang-Lei-NOAA"]
 
-    version("2.22.0", sha256="3356b8d29813431d272c5464e265f3fe3ce1ac7f49ae6d41da34fe4b82aa691a")
+    version('develop', branch='develop')
+    version('main', branch='main')
+
+    version("2.26.0", sha256="4ef9a1eeffc0521bd6fb4ea5ab261e07d3b5a46f0a6c43673ee169bf0d624bb8")
+    version("2.25.0", sha256="f3ce71004f4274cedee28852cc105e0bf51a86cafc4d5f5af9de6492c4be9402")
+    version("2.24.0", sha256='cd15ffd6897c18e64267e5fa86523402eb48cbf638ad5f3b4b5b0ff8939d1936')
+    version("2.23.1", sha256='563f3e9f33adae298835e7de7a4a29452a2a584d191248c59494c49d3ee80d24')
+    version("2.23.0", sha256='ae25ec63d0f288599c668f35fdbccc76abadbfc6d48f95b6eb4e7a2c0c69f241')
+    version("2.22.0", sha256='3356b8d29813431d272c5464e265f3fe3ce1ac7f49ae6d41da34fe4b82aa691a')
     version("2.12.3", sha256="e849eff291939509e74830f393cb2670c2cc96f6160d8060dbeb1742639c7d41")
     version("2.11.0", sha256="76351e026c17e2044b89085db639e05ba0e7439a174d14181e01874f0f93db44")
     version("2.8.1", sha256="a7657d4c52a66c3a6663e436d2c2dd4dbb81addd747e1ace68f59843665eb739")
@@ -56,12 +65,16 @@ class Mapl(CMakePackage):
     # Patch to add missing MPI Fortran target to top-level CMakeLists.txt
     patch("mapl-2.12.3-mpi-fortran.patch", when="@:2.12.3")
 
-    variant("flap", default=False)
-    variant("pflogger", default=False)
-    variant("esma_gfe_namespace", default=True)
-    variant("shared", default=True)
+    variant('flap', default=False, description="Build with FLAP support")
+    variant('pflogger', default=False, description="Build with pFlogger support")
+    variant('shared', default=True, description="Build as shared library")
     variant("debug", default=False, description="Make a debuggable version of the library")
     variant("extdata2g", default=False, description="Use ExtData2G")
+    variant("pfunit", default=False, description="Build with pFUnit support")
+
+    variant('build_type', default='Release',
+        description='The build type to build',
+        values=('Debug', 'Release', 'Aggressive'))
 
     depends_on("cmake@3.17:")
     depends_on("mpi")
@@ -72,15 +85,30 @@ class Mapl(CMakePackage):
     depends_on("esmf", when="@:2.12.99")
     depends_on("esmf~debug", when="~debug")
     depends_on("esmf+debug", when="+debug")
-    depends_on("yafyaml@:0.5.1")
-    depends_on("gftl-shared@1.3.1:")
+    
+    depends_on('gftl@1.5.5:')
+    depends_on('gftl-shared@1.3.1:')
+
+    # There was an interface change in yaFyaml, so we need to control versions
+    # MAPL 2.22 and older uses older version, MAPL 2.23+ and higher uses newer
+    depends_on('yafyaml@1.0-beta5', when='@:2.22+extdata2g')
+    depends_on('yafyaml@1.0.4:', when='@2.23:+extdata2g')
+
+    # pFlogger depends on yaFyaml in the same way. MAPL 2.22 and below uses old
+    # yaFyaml so we need to use old pFlogger, but MAPL 2.23+ uses new yaFyaml
+    depends_on('pflogger@:1.6', when='@:2.22+pflogger')
+    depends_on('pflogger@1.9.1:', when='@2.23:+pflogger')        
+
+    depends_on('pfunit@4.2:, when='+pfunit')
+    depends_on('flap', when='+flap')
+
+    
     depends_on("ecbuild")
 
     def cmake_args(self):
         args = [
             self.define_from_variant("BUILD_WITH_FLAP", "flap"),
             self.define_from_variant("BUILD_WITH_PFLOGGER", "pflogger"),
-            self.define_from_variant("ESMA_USE_GFE_NAMESPACE", "esma_gfe_namespace"),
             self.define_from_variant("BUILD_SHARED_MAPL", "shared"),
             self.define_from_variant("USE_EXTDATA2G", "extdata2g"),
             "-DCMAKE_C_COMPILER=%s" % self.spec["mpi"].mpicc,
