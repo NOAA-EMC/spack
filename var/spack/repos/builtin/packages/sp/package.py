f2b9f7f578df1f73078c240cf17bafd7b5efd91c
Alex Richert <alexander.richert@noaa.gov>
2023-08-02 00:52:53 -0700

    Update sp recipe

--- var/spack/repos/builtin/packages/sp/package.py
+++ var/spack/repos/builtin/packages/sp/package.py
@@ -16,22 +16,42 @@ class Sp(CMakePackage):
 
     maintainers("t-brown", "AlexanderRichert-NOAA", "edwardhartnett", "Hang-Lei-NOAA")
 
+    version("2.4.0", sha256="dbb4280e622d2683b68a28f8e3837744adf9bbbb1e7940856e8f4597f481c708")
     version("2.3.3", sha256="c0d465209e599de3c0193e65671e290e9f422f659f1da928505489a3edeab99f")
 
-    variant("openmp", default=True,
-            description="builds with OpenMP support")
-    variant("pic", default=True, description="Build with position-independent-code")
+    variant("shared", default=False, description="Build shared library", when="@2.4:")
+    variant("openmp", default=False, description="Use OpenMP threading")
+    variant("pic", default=False, description="Enable position-independent code (PIC)")
+    variant(
+        "precision",
+        default=["4", "d"],
+        values=["4", "d", "8"],
+        multi=True,
+        description="Library versions: 4=4-byte reals, d=8-byte reals, 8=8-byte ints and reals",
+        when="@2.4:",
+    )
 
     def setup_run_environment(self, env):
-        for suffix in ("4", "8", "d"):
-            lib = find_libraries("libsp_" + suffix, root=self.prefix,
-                                 shared=False, recursive=True)
+        if self.spec.satisfies("@2.4:"):
+            suffixes = self.spec.variants["precision"].value
+        else:
+            suffixes = ["4", "d", "8"]
+        for suffix in suffixes:
+            lib = find_libraries(
+                "libsp_" + suffix,
+                root=self.prefix,
+                shared=self.spec.satisfies("+shared"),
+                recursive=True,
+            )
             env.set("SP_LIB" + suffix, lib[0])
             env.set("SP_INC" + suffix, "include_" + suffix)
 
     def cmake_args(self):
-        args = [
-            self.define_from_variant("OPENMP", "openmp"),
-            self.define_from_variant("CMAKE_POSITION_INDEPENDENT_CODE", "pic")
-        ]
+        args = []
+        args.append(self.define_from_variant("BUILD_SHARED_LIBS", "shared"))
+        args.append(self.define_from_variant("CMAKE_POSITION_INDEPENDENT_CODE", "pic"))
+        args.append(self.define_from_variant("OPENMP", "openmp"))
+        args.append(self.define("BUILD_4", self.spec.satisfies("precision=4")))
+        args.append(self.define("BUILD_D", self.spec.satisfies("precision=d")))
+        args.append(self.define("BUILD_8", self.spec.satisfies("precision=8")))
         return args
