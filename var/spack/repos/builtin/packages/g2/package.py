20ff61bf6a987a7d3aa28e6b60186f338d1088ed
Alex Richert <alexander.richert@noaa.gov>
2023-08-01 23:27:02 -0700

    Add v3.4.6 to g2 recipe

--- var/spack/repos/builtin/packages/g2/package.py
+++ var/spack/repos/builtin/packages/g2/package.py
@@ -15,20 +15,27 @@ class G2(CMakePackage):
 
     homepage = "https://noaa-emc.github.io/NCEPLIBS-g2"
     url = "https://github.com/NOAA-EMC/NCEPLIBS-g2/archive/refs/tags/v3.4.3.tar.gz"
+    git = "https://github.com/NOAA-EMC/NCEPLIBS-g2"
 
     maintainers("t-brown", "AlexanderRichert-NOAA", "Hang-Lei-NOAA", "edwardhartnett")
 
+    version("develop", branch="develop")
+    version("3.4.6", sha256="c4b03946365ce0bacf1e10e8412a5debd72d8671d1696aa4fb3f3adb119175fe")
     version("3.4.5", sha256="c18e991c56964953d778632e2d74da13c4e78da35e8d04cb742a2ca4f52737b6")
     version("3.4.3", sha256="679ea99b225f08b168cbf10f4b29f529b5b011232f298a5442ce037ea84de17c")
 
     variant("pic", default=True, description="Build with position-independent-code")
+    variant("w3emc", default=True, description="Enable GRIB1 through w3emc", when="@3.4.6:")
 
     depends_on("jasper@:2.0.32")
     depends_on("libpng")
+    depends_on("bacio", when="@3.4.6:")
+    depends_on("w3emc", when="@3.4.6:")
 
     def cmake_args(self):
         args = [
-            self.define_from_variant("CMAKE_POSITION_INDEPENDENT_CODE", "pic")
+            self.define_from_variant("CMAKE_POSITION_INDEPENDENT_CODE", "pic"),
+            self.define_from_variant("BUILD_WITH_W3EMC", "w3emc"),
         ]
 
         return args
