58e01c26343887ff03da25d6e0196a230c853d5b
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-11-21 20:47:05 -0700

    Add geos-gcm-env

---
+++ var/spack/repos/jcsda-emc-bundles/packages/geos-gcm-env/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class GeosGcmEnv(BundlePackage):
+    """Development environment for GEOS-GCM"""
+
+    homepage = "https://gmao.gsfc.nasa.gov/GEOS_systems"
+    git = "https://github.com/GEOS-ESM/GEOSgcm"
+
+    maintainers("climbfuji", "mathomp4", "danholdaway")
+
+    # Current version
+    version("1.0.0")
+
+    depends_on("base-env", type="run")
+    depends_on("mepo", type="run")
+
+    depends_on("esmf", type="run")
+    depends_on("mapl+pflogger+f2py", type="run")
+
+    # There is no need for install() since there is no code.
